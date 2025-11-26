849fc0813761f48e141d2fce7bf2ff24e28878d9
ElyasShamal <Elyas.shamal2@gmail.com>
2025-11-25 22:36:41 -0500

    add sec solution

--- index.js
+++ index.js
@@ -5,3 +5,15 @@ function superbowlWin(record){
     const winner = record.find(game => game.result === "W")
     return winner ? winner.year : undefined
 }
+
+
+// Solution two
+
+// function superbowlWin(record){
+//     for(const game of record){
+//         if (game.result === "W"){
+//             return game.year
+//         }
+//     }
+//     return undefined
+// }
